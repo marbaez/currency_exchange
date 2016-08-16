c9c9e98902129366ac9d2cacc2b6e3ad85aaff99
Marcos <marbaez@gmail.com>
2016-08-16 15:01:26 +0200

    empezando con los test de los controladores

--- src/test/java/com/marbaez/currency/CurrencyExchangeApplicationTests.java
+++ src/test/java/com/marbaez/currency/CurrencyExchangeApplicationTests.java
@@ -2,15 +2,29 @@ package com.marbaez.currency;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(
+        webEnvironment = WebEnvironment.RANDOM_PORT)
 public class CurrencyExchangeApplicationTests {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private final String wrongUrl = "/notFound";
+
     @Test
-    public void contextLoads() {
+    public void notFoundURL() {
+
+        final String body = this.restTemplate.getForObject(wrongUrl, String.class);
+        assertThat(body).contains("404");
+        assertThat(body).contains("Not Found");
     }
 
 }
